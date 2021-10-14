d9cc5e134ccbdf803785274310b65c4af595b315
Hansatang <portokarlos@gmail.com>
2021-10-14 11:25:42 +0200

    Initial commit

---
+++ ServerVersion2/ClientRest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServerVersion2
+{
+    public class ClientRest
+    {
+        private string uri = "http://localhost:8080";
+        private readonly HttpClient client;
+
+        public ClientRest()
+        {
+            client = new HttpClient();
+        }
+        
+        public async Task<IList<String>> GetAdultAsync()
+        {
+            Console.WriteLine("Hi");
+            Task<string> stringAsync = client.GetStringAsync(uri + "/BankF/1234");
+            string message = await stringAsync;
+            List<String> result = JsonSerializer.Deserialize<List<String>>(message);
+            Console.WriteLine("Hi2");
+            return result;
+        }
+    }
+}
